7e3b065669461c032abc23c34fa3a6b7ef18f56f
RyotaFukumoto <k014c1106@it-neec.jp>
2021-11-23 14:48:11 +0900

    Initial Commit

---
+++ RxSwiftDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftDemoApp
+//
+//  Created by ryota on 2021/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
